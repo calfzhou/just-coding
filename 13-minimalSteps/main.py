b5ce50a0f2bfd94064515a27f4325ea265b25d5d
Ji Zhou <calf.zhou@gmail.com>
2020-12-09 17:41:15 +0800

    add 13 not done

---
+++ 13-minimalSteps/main.py
@@ -0,0 +1,22 @@
+from typing import *
+from math import *
+
+
+# TODO
+
+class Solution(object):
+    def minimalSteps(self, maze):
+        """
+        :type maze: List[str]
+        :rtype: int
+        """
+        pass
+
+
+sol = Solution()
+data = ["S#O", "M..", "M.T"] # 16
+# data = ["S#O", "M.#", "M.T"] # -1
+# data = ["S#O", "M.T", "M.."] # 17
+print(data)
+res = sol.minimalSteps(data)
+print(res)
